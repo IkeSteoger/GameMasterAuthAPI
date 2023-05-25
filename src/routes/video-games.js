321c540264a4168b50fec810a28295eaa741a5b8
Tim Maupin <timmaupin@live.com>
2023-05-25 18:06:50 -0500

    add a try catch to each of the video game functions so the server just shutdown through an invalid user error

--- src/routes/video-games.js
+++ src/routes/video-games.js
@@ -8,35 +8,52 @@ const bearerAuth = require('../auth/middleware/bearer');
 const acl = require('../auth/middleware/acl');
 
 router.post('/videoGames', bearerAuth, acl('create'), async (request, response, next) => {
-  let videoGames = await videoGamesModel.create(request.body);
+  try {
+    let videoGames = await videoGamesModel.create(request.body);
 
-  response.status(200).send(videoGames);
+    response.status(200).send(videoGames);
+  } catch (err) {
+    next(err);
+  }
 });
 
 router.get('/videoGames', basicAuth, async (request, response, next) => {
-  let videoGames = await videoGamesModel.read();
+  try {
+    let videoGames = await videoGamesModel.read();
 
-  response.status(200).send(videoGames);
+    response.status(200).send(videoGames);
+  } catch (error) {
+    next(error);
+  }
 });
 
 router.get('/videoGames/:id', basicAuth, async (request, response, next) => {
-  let videoGames = await videoGamesModel.read(request.params.id);
+  try {
+    let videoGames = await videoGamesModel.read(request.params.id);
 
-  response.status(200).send(videoGames);
+    response.status(200).send(videoGames);
+  } catch (error) {
+    next(error);
+  }
 });
 
 router.put('/videoGames/:id', bearerAuth, acl('update'), async (request, response, next) => {
-  await videoGamesModel.update(request.body, request.params.id);
-  let updatedvideoGames = await videoGamesModel.read(request.params.id);
-  response.status(200).send(updatedvideoGames);
+  try {
+    await videoGamesModel.update(request.body, request.params.id);
+    let updatedvideoGames = await videoGamesModel.read(request.params.id);
+    response.status(200).send(updatedvideoGames);
+  } catch (error) {
+    next(error);
+  }
+
 });
 
 router.delete('/videoGames/:id', bearerAuth, acl('delete'), async (request, response, next) => {
   try {
     const deletedGame = await videoGamesModel.delete(request.params.id);
     response.status(200).send(deletedGame);
-  } catch(err) {
-    next(err);
+  } catch (error) {
+    next(error);
   }
 });
 
