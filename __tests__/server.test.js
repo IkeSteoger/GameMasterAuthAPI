a3ff5a7b90782685adda45b1a8e74687a0dc9a3b
MICHAEL STEOGER <ikesteoger@gmail.com>
2023-05-25 16:20:44 -0700

    tests finished up

--- __tests__/server.test.js
+++ __tests__/server.test.js
@@ -11,8 +11,8 @@ beforeAll(async () => {
   await db.sync();
   adminTest = await users.create({
     username: 'admin',
-    password: 'admin',
-    role: 'admin',
+    password: 'pass',
+    role: 'gameMaster',
   });
 });
 
@@ -48,7 +48,7 @@ describe('Auth Routes', () => {
     const response = await request.post('/signup').send({
       username: 'Tester', 
       password: 'pass',
-      role: 'admin',
+      role: 'gameMaster',
     });
 
     expect(response.status).toEqual(201);
@@ -60,5 +60,80 @@ describe('Auth Routes', () => {
     expect(response.status).toEqual(200);
   });
 
+  test('read all /videogames - requires basic auth', async () => {
+    const response = await request.get('/videogames').set('Authorization', 'Basic VGVzdGVyOnBhc3M=');
+
+    expect(response.status).toEqual(200);
+  });
+
+  test('read one /videogames/:id - requires basic auth', async () => {
+    const response = await request.get('/videogames/1').set('Authorization', 'Basic VGVzdGVyOnBhc3M=');
+
+    expect(response.status).toEqual(200);
+  });
+
+  test('create /videogames - requires bearer auth & acl-create', async () => {
+    const response = await request.post('/videogames').send({
+      name: 'halo',
+      released: '2001',
+      genre: 'fps',
+    }).set('Authorization', `Bearer ${adminTest.token}`);
+
+    expect(response.status).toEqual(200);
+  });
+
+  test('update /videogames/:id - requires bearer auth & acl-update', async () => {
+    const response = await request.put('/videogames/1').send({
+      name: 'halo 2',
+      released: '2004',
+      genre: 'fps 2.0',
+    }).set('Authorization', `Bearer ${adminTest.token}`);
+
+    expect(response.status).toEqual(200);
+  });
+
+  test('delete /videogames/:id - requires bearer auth & acl-delete', async () => {
+    const response = await request.delete('/videogames/1').set('Authorization', `Bearer ${adminTest.token}`);
+
+    expect(response.status).toEqual(200);
+  });
+
+  test('read all /tabletopGames - requires basic auth', async () => {
+    const response = await request.get('/tabletopGames').set('Authorization', 'Basic VGVzdGVyOnBhc3M=');
+
+    expect(response.status).toEqual(200);
+  });
+
+  test('read one /tabletopGames/:id - requires basic auth', async () => {
+    const response = await request.get('/tabletopGames/1').set('Authorization', 'Basic VGVzdGVyOnBhc3M=');
+
+    expect(response.status).toEqual(200);
+  });
+
+  test('create /tabletopGames - requires bearer auth & acl-create', async () => {
+    const response = await request.post('/tabletopGames').send({
+      name: 'catan',
+      released: '1998',
+      genre: 'fun',
+    }).set('Authorization', `Bearer ${adminTest.token}`);
+
+    expect(response.status).toEqual(200);
+  });
+
+  test('update /tabletopGames/:id - requires bearer auth & acl-update', async () => {
+    const response = await request.put('/tabletopGames/1').send({
+      name: 'halo 2',
+      released: '2004',
+      genre: 'fps 2.0',
+    }).set('Authorization', `Bearer ${adminTest.token}`);
+
+    expect(response.status).toEqual(200);
+  });
+
+  test('delete /tabletopGames/:id - requires bearer auth & acl-delete', async () => {
+    const response = await request.delete('/tabletopGames/1').set('Authorization', `Bearer ${adminTest.token}`);
+
+    expect(response.status).toEqual(200);
+  });
 
 });
