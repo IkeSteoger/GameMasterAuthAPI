10a68fca9c50c6e2998fee7471b9dc8003fea488
Tim Maupin <timmaupin@live.com>
2023-05-25 17:24:59 -0500

    adding basicAuth, bearerAuth and acl to videogames routes

--- src/routes/video-games.js
+++ src/routes/video-games.js
@@ -3,32 +3,35 @@
 const express = require('express');
 const router = express.Router();
 const { videoGamesModel } = require('../models/index');
+const basicAuth = require('../auth/middleware/basic');
+const bearerAuth = require('../auth/middleware/bearer');
+const acl = require('../auth/middleware/acl');
 
-router.post('/videoGames', async (request, response, next) => {
+router.post('/videoGames', bearerAuth, acl('create'), async (request, response, next) => {
   let videoGames = await videoGamesModel.create(request.body);
 
   response.status(200).send(videoGames);
 });
 
-router.get('/videoGames', async (request, response, next) => {
+router.get('/videoGames', basicAuth, async (request, response, next) => {
   let videoGames = await videoGamesModel.read();
 
   response.status(200).send(videoGames);
 });
 
-router.get('/videoGames/:id', async (request, response, next) => {
+router.get('/videoGames/:id', basicAuth, async (request, response, next) => {
   let videoGames = await videoGamesModel.read(request.params.id);
 
   response.status(200).send(videoGames);
 });
 
-router.put('/videoGames/:id', async (request, response, next) => {
+router.put('/videoGames/:id', bearerAuth, acl('update'), async (request, response, next) => {
   await videoGamesModel.update(request.body, request.params.id);
   let updatedvideoGames = await videoGamesModel.read(request.params.id);
   response.status(200).send(updatedvideoGames);
 });
 
-router.delete('/videoGames/:id', async (request, response, next) => {
+router.delete('/videoGames/:id', bearerAuth, acl('delete'), async (request, response, next) => {
   try {
     const deletedGame = await videoGamesModel.delete(request.params.id);
     response.status(200).send(deletedGame);
