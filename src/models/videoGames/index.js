2d61f3eb27976a2b24c01471d1e6ccd61c8642eb
Tim Maupin <timmaupin@live.com>
2023-05-25 17:53:56 -0500

    fix issues with the collections routes, gave tabletopgames and videogames acces to database to create and get data

--- src/models/videoGames/index.js
+++ src/models/videoGames/index.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = (sequelizeDatabase, DataTypes) => {
-  return sequelizeDatabase.define('tabletopGames', {
+  return sequelizeDatabase.define('videoGames', {
     name: {
       type: DataTypes.STRING,
       allowNull: false,
